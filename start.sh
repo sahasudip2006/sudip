7d457ff646984db2e0e4179cf238e31184d50725
parshucr123 <118430657+parshucr123@users.noreply.github.com>
2023-10-01 16:04:44 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/VJ-FILTER-BOT.git /VJ-FILTER-BOT 
+  git clone https://github.com/parshucr123/FASTDOWNLOAD-BOT.git /FASTDOWNLOAD-BOT 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /VJ-FILTER-BOT 
+  git clone $UPSTREAM_REPO /FASTDOWNLOAD-BOT 
 fi
-cd /VJ-FILTER-BOT 
+cd /FASTDOWNLOAD-BOT 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
